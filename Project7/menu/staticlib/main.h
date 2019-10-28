45225f1082ace8e312f42f433d6160aa597c36d7
NNihilism <405307400@qq.com>
2019-10-28 08:06:09 +0800

    静态库的生成

---
+++ Project7/menu/staticlib/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+void showMenu_1();
+void createFile_1(char* fileName);
+void writeFile_1(char* fileName);
+void readFile_1(char* fileName);
+void changeRight_1(char* fileName);
+void readRight_1(char* fileName);
