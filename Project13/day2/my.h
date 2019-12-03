dfcd5888634099fe437f3e7eece4b0def280cf75
NNihilism <405307400@qq.com>
2019-12-03 15:31:28 +0800

    week13

---
+++ Project13/day2/my.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<signal.h>
+#include<wait.h>
+#include<string.h>
+#include<errno.h>
